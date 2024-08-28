7d15e9ad0ea7d1ed5ebb1ae910c749fcc7aa2ecb
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-08-28 21:51:30 +0100

    add dynamic content

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+const reactDescriptions = ['Core', 'Fundamental', 'Crucial'];
+
+function generateRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+var description = reactDescriptions[generateRandomInt(2)];
+
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
