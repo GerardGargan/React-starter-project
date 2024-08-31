738f24c0cd418e3e432879a0bf0a1bc085ff7d1a
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-08-31 11:40:36 +0100

    add dynamic tab styling

--- src/App.jsx
+++ src/App.jsx
@@ -44,10 +44,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton isSelected={selectedText==='components'} onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton isSelected={selectedText==='jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton isSelected={selectedText==='props'} onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton isSelected={selectedText==='state'} onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
         </section>
         {tabContent}
